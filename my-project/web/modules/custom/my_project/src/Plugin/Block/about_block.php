f230848abb231af3e8b90c528b37359a8d9add5b
Adrienne Dao <adriennedao@gmail.com>
2018-05-27 03:19:54 +0000

    combined projects

---
+++ my-project/web/modules/custom/my_project/src/Plugin/Block/about_block.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\my_project\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'about_block' block.
+ *
+ * @Block(
+ *  id = "about_block",
+ *  admin_label = @Translation("About_block"),
+ * )
+ */
+class about_block extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+    $build['about_block']['#markup'] = 'Implement about_block.';
+
+    return $build;
+  }
+
+}
