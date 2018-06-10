b471e5756413ea9eb92374bcc4612964824cb71b
adriennedao <adriennedao@gmail.com>
2018-06-10 08:13:09 +0000

    added form validation

--- php/form.php
+++ php/form.php
@@ -2,12 +2,37 @@
 
 if(isset ($_POST['submit'])) {
 
-	echo "yes it works!";
+	$name = array("Edwin", "Student", "adrienne", "jonh", "jeff", "james", "maria");
+	$minimum = 5;
+	$maximum = 10;
 
+$username =_POST['username'];
+$password =_POST['password'];
 
+	
+//echo "Hello" . $username;
+//echo "Your Password is " .$password;
+
+	if(strlen($username) < minimum) {
+
+		echo "Username has to be longer than five";
+	
 }
 
+	if(strlen($username) > maximum) {
 
+		echo "Username cannot be longer than ten";
+
+}
+	if(!in_array($username,$name)) {
+
+		echo "Sorry you are not allowed";
+
+} else {
+
+	echo "Welcome";
+
+}
 
 
 ?>
@@ -23,8 +48,8 @@ if(isset ($_POST['submit'])) {
 
 <form action="form.php" method="post">
 
-<input type="text" placeholder="Enter Username">
-<input type="password" placeholder="Enter Password">
+<input type="text" name="username" placeholder="Enter Username">
+<input type="password" name="password" placeholder="Enter Password">
 <br>
 
 <input type="submit" name="submit">
