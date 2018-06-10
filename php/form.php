b9401bedc3ca42ab6b604f135170725c5ece9a20
adriennedao <adriennedao@gmail.com>
2018-06-10 07:09:47 +0000

    created form

---
+++ php/form.php
@@ -0,0 +1,35 @@
+<?php
+
+if(isset ($_POST['submit'])) {
+
+	echo "yes it works!";
+
+
+}
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="form.php" method="post">
+
+<input type="text" placeholder="Enter Username">
+<input type="password" placeholder="Enter Password">
+<br>
+
+<input type="submit" name="submit">
+</form>
+
+
+</body>
+</html>
