9d326a0ec674f39d4d117d7562199211948351d9
adriennedao <adriennedao@gmail.com>
2018-06-10 06:47:15 +0000

    created array_functions

---
+++ php/math_functions.php
@@ -0,0 +1,28 @@
+<?php
+
+print '<h1>My PHP File</h1>';
+print '<br><br>';
+
+echo pow(2, 7);
+
+echo "<br>";
+
+echo rand(1, 1000);
+
+echo pow(2, 7);
+
+echo "<br>";
+
+echo sqrt(100);
+
+echo "<br>";
+
+echo ceil(4.6);
+
+echo "<br>";
+
+echo floor(4.6);
+
+echo "<br>";
+
+echo round(4.5);
