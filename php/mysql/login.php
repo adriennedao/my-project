22a85d515da4325f598169dd25d54dfc1b562a54
AdrienneD <adriennedao@gmail.com>
2018-06-18 02:12:44 -0400

    connected submit form to sqldatabase

--- php/mysql/login.php
+++ php/mysql/login.php
@@ -1,3 +1,37 @@
+<?php 
+
+if(isset($_POST['submit'])) {
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+$connection = mysqli_connect('localhost', 'root', '', 'loginapp');
+
+	if($connection) {
+
+		echo "We are connected";
+
+	} else {
+
+		die("Database connection failed");
+
+	}
+
+ }
+	//if($username && $password) {
+
+	//echo $username;
+	//echo $password;
+
+
+	//} else {
+
+	//	echo "This field cannot be blanked";
+//	}
+// }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,8 +42,8 @@
 
 	<div class="container">
 
-		<div class="col-xs-6">
-			<form action="login.php" method=""post">
+		<div class="col-sm-6">
+			<form action="login.php" method="post">
 				<div class="form-group">
 					<label for="username">Username</label>
 					<input type="text" name="username" class="form-control">
@@ -27,5 +61,4 @@
 
 </body>
 </html>
-
 
