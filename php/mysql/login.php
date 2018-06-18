412d10a2307cb29804064bc33191599f18d2e9b5
AdrienneD <adriennedao@gmail.com>
2018-06-18 01:27:16 -0400

    created login form

---
+++ php/mysql/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
+<body>
+
+	<div class="container">
+
+		<div class="col-xs-6">
+			<form action="login.php" method=""post">
+				<div class="form-group">
+					<label for="username">Username</label>
+					<input type="text" name="username" class="form-control">
+				</div>
+
+				<div class="form-group">
+					<label for="password">Password</label>
+					<input type="password" name="password" class="form-control">
+				</div>
+
+				<input class="btn btn-primary" type="submit" name="submit" value="Submit">
+			</form>
+		</div>
+
+
+</body>
+</html>
+
+
